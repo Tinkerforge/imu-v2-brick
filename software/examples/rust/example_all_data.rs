82875eb087486c110b5bb2b452238f07784ff483
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:53:44 +0100

    rust: Reformat examples.

--- software/examples/rust/example_all_data.rs
+++ software/examples/rust/example_all_data.rs
@@ -19,94 +19,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for all_data in all_data_receiver {
-            println!(
-                "Acceleration [X]: {} m/s²",
-                all_data.acceleration[0] as f32 / 100.0
-            );
-            println!(
-                "Acceleration [Y]: {} m/s²",
-                all_data.acceleration[1] as f32 / 100.0
-            );
-            println!(
-                "Acceleration [Z]: {} m/s²",
-                all_data.acceleration[2] as f32 / 100.0
-            );
-            println!(
-                "Magnetic Field [X]: {} µT",
-                all_data.magnetic_field[0] as f32 / 16.0
-            );
-            println!(
-                "Magnetic Field [Y]: {} µT",
-                all_data.magnetic_field[1] as f32 / 16.0
-            );
-            println!(
-                "Magnetic Field [Z]: {} µT",
-                all_data.magnetic_field[2] as f32 / 16.0
-            );
-            println!(
-                "Angular Velocity [X]: {} °/s",
-                all_data.angular_velocity[0] as f32 / 16.0
-            );
-            println!(
-                "Angular Velocity [Y]: {} °/s",
-                all_data.angular_velocity[1] as f32 / 16.0
-            );
-            println!(
-                "Angular Velocity [Z]: {} °/s",
-                all_data.angular_velocity[2] as f32 / 16.0
-            );
-            println!(
-                "Euler Angle [X]: {} °",
-                all_data.euler_angle[0] as f32 / 16.0
-            );
-            println!(
-                "Euler Angle [Y]: {} °",
-                all_data.euler_angle[1] as f32 / 16.0
-            );
-            println!(
-                "Euler Angle [Z]: {} °",
-                all_data.euler_angle[2] as f32 / 16.0
-            );
-            println!(
-                "Quaternion [W]: {}",
-                all_data.quaternion[0] as f32 / 16383.0
-            );
-            println!(
-                "Quaternion [X]: {}",
-                all_data.quaternion[1] as f32 / 16383.0
-            );
-            println!(
-                "Quaternion [Y]: {}",
-                all_data.quaternion[2] as f32 / 16383.0
-            );
-            println!(
-                "Quaternion [Z]: {}",
-                all_data.quaternion[3] as f32 / 16383.0
-            );
-            println!(
-                "Linear Acceleration [X]: {} m/s²",
-                all_data.linear_acceleration[0] as f32 / 100.0
-            );
-            println!(
-                "Linear Acceleration [Y]: {} m/s²",
-                all_data.linear_acceleration[1] as f32 / 100.0
-            );
-            println!(
-                "Linear Acceleration [Z]: {} m/s²",
-                all_data.linear_acceleration[2] as f32 / 100.0
-            );
-            println!(
-                "Gravity Vector [X]: {} m/s²",
-                all_data.gravity_vector[0] as f32 / 100.0
-            );
-            println!(
-                "Gravity Vector [Y]: {} m/s²",
-                all_data.gravity_vector[1] as f32 / 100.0
-            );
-            println!(
-                "Gravity Vector [Z]: {} m/s²",
-                all_data.gravity_vector[2] as f32 / 100.0
-            );
+            println!("Acceleration [X]: {} m/s²", all_data.acceleration[0] as f32 / 100.0);
+            println!("Acceleration [Y]: {} m/s²", all_data.acceleration[1] as f32 / 100.0);
+            println!("Acceleration [Z]: {} m/s²", all_data.acceleration[2] as f32 / 100.0);
+            println!("Magnetic Field [X]: {} µT", all_data.magnetic_field[0] as f32 / 16.0);
+            println!("Magnetic Field [Y]: {} µT", all_data.magnetic_field[1] as f32 / 16.0);
+            println!("Magnetic Field [Z]: {} µT", all_data.magnetic_field[2] as f32 / 16.0);
+            println!("Angular Velocity [X]: {} °/s", all_data.angular_velocity[0] as f32 / 16.0);
+            println!("Angular Velocity [Y]: {} °/s", all_data.angular_velocity[1] as f32 / 16.0);
+            println!("Angular Velocity [Z]: {} °/s", all_data.angular_velocity[2] as f32 / 16.0);
+            println!("Euler Angle [X]: {} °", all_data.euler_angle[0] as f32 / 16.0);
+            println!("Euler Angle [Y]: {} °", all_data.euler_angle[1] as f32 / 16.0);
+            println!("Euler Angle [Z]: {} °", all_data.euler_angle[2] as f32 / 16.0);
+            println!("Quaternion [W]: {}", all_data.quaternion[0] as f32 / 16383.0);
+            println!("Quaternion [X]: {}", all_data.quaternion[1] as f32 / 16383.0);
+            println!("Quaternion [Y]: {}", all_data.quaternion[2] as f32 / 16383.0);
+            println!("Quaternion [Z]: {}", all_data.quaternion[3] as f32 / 16383.0);
+            println!("Linear Acceleration [X]: {} m/s²", all_data.linear_acceleration[0] as f32 / 100.0);
+            println!("Linear Acceleration [Y]: {} m/s²", all_data.linear_acceleration[1] as f32 / 100.0);
+            println!("Linear Acceleration [Z]: {} m/s²", all_data.linear_acceleration[2] as f32 / 100.0);
+            println!("Gravity Vector [X]: {} m/s²", all_data.gravity_vector[0] as f32 / 100.0);
+            println!("Gravity Vector [Y]: {} m/s²", all_data.gravity_vector[1] as f32 / 100.0);
+            println!("Gravity Vector [Z]: {} m/s²", all_data.gravity_vector[2] as f32 / 100.0);
             println!("Temperature: {} °C", all_data.temperature);
             println!("Calibration Status: {:b}", all_data.calibration_status);
             println!();
